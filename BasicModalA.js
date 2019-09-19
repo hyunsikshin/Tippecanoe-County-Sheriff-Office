fe9c64fc30a25ff3c98f89629b355fcb9e30501b
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2019-09-19 10:15:52 -0400

    모달 업데이트

--- BasicModalA.js
+++ BasicModalA.js
@@ -1,36 +1,87 @@
 import React, { Component } from 'react';
 import Modal from 'react-native-simple-modal';
-import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet,Button } from 'react-native';
 import { Linking } from 'expo';
 import ControllContainer from './actions/ControllContainer';
+import getLink from './actions/getLink';
 
 export default class BasicModalA extends Component {
   render() {
-    const modalBackgroundStyle = {
-      backgroundColor: 'rgba(103, 89, 71, 50)',
-    };
-
     return (
-      <Modal
-      open ={true}
-      offset = { 0 }
-      style={{ modalBackgroundStyle , alignItems: 'center' }}
-      modalDidClose={() => ControllContainer.getInstance().closeModal()}
-      >
-      <View style={{ alignItems: 'center' }}>
-        <Text style={{ fontSize: 20, marginBottom: 10 }}>Contact</Text>
-        <TouchableOpacity
-          style={{ margin: 10, fontSize: 20, alignItems: 'center' }}
-          onPress={() => Linking.openURL('tel:7654239388')}>
-          <Text>모달 테스트 2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{ margin: 10, alignItems: 'center' }}
-          onPress={() => ControllContainer.getInstance().closeModal()}>
-          <Text>Close</Text>
-        </TouchableOpacity>
-      </View>
-    </Modal>
+        <Modal
+        style={styles.container}
+          open={true}
+          transparent={true}
+          animationType='slide'
+          modalDidClose={() => ControllContainer.getInstance().closeModal()}>
+              
+            <View style={[styles.container, styles.modalBackgroundStyle]}>
+                <View style={styles.Cell_Title}>
+                    <Text style ={styles.TitleText}>Contact</Text>
+               </View>
+            
+            <View style={{alignItems:'center'}}>
+                <TouchableOpacity style = {styles.InnerText}
+                onPress={() =>  getLink.getLink('Lafayette','administration')}>
+                <Text style ={styles.InnerText}>ADMINISTRATION</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style = {styles.InnerText}
+                onPress={() => getLink.getLink('Lafayette','dispatch')}>
+                <Text style ={styles.InnerText}>DISPATCH</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style = {styles.InnerText}
+                onPress={() => getLink.getLink('Lafayette','jail')}>
+                <Text style ={styles.InnerText}>JAIL</Text>
+                </TouchableOpacity>
+
+                <View style={styles.Cell_Close}>
+                    <TouchableOpacity
+                    style={{alignItems: 'center' }}
+                    onPress={() =>  ControllContainer.getInstance().closeModal()}>
+                    <Text style ={{fontSize: 30}}>CLOSE</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+     </View>
+        </Modal>
     );
   }
 }
+
+const styles = StyleSheet.create({
+
+  TitleText:{
+      color:'white',
+      alignItems:'center',
+    fontSize:40 
+  },
+
+  InnerText:{
+    fontSize: 40,
+    color:'white',
+    textDecorationLine: 'underline',
+  },
+
+  Cell_Title: {
+    width: 330,
+    height: 55,
+    margin: 5,
+    backgroundColor: '#55330E',
+    alignItems:'center',
+  },
+
+  Cell_Close: {
+    width: 300,
+    height: 45,
+    marginVertical: 10,
+    backgroundColor: '#D6D8D7'
+  },
+   modalBackgroundStyle : {
+    backgroundColor: 'rgba(78,62,46, 1)'
+    ,margin : -10
+    ,alignItems:'center'
+  }
+
+});
