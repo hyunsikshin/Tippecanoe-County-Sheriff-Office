3af932621dacd5c90f4336ccafd73362903487c7
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2019-10-11 13:10:53 -0400

    ViewContainer 수정 및 buttonScreen 삭제

--- actions/ViewContainer.js
+++ actions/ViewContainer.js
@@ -1,23 +1,11 @@
 import React from 'react';
 
-import ButtonSet from '../components/ButtonSet';
-import CorrectionSet from '../components/CorrectionSet';
-import ServiceSet from '../components/ServiceSet';
-import SnsSet from '../components/SnsSet';
-
 import BasicModal from '../dialog/BasicModal';
 import BasicModalA from '../dialog/BasicModalA';
 import BasicModalB from '../dialog/BasicModalB';
 import BasicModalC from '../dialog/BasicModalC';
 import BasicModalD from '../dialog/BasicModalD';
 
-
-const buttonList = {
-      Main :<ButtonSet></ButtonSet>,
-      Corrections :<CorrectionSet></CorrectionSet>,
-      Service :<ServiceSet></ServiceSet>,
-      SocialMedia :<SnsSet></SnsSet>,
-  };
 
 const modallist = {
     BasicModal :<BasicModal></BasicModal>,
@@ -28,7 +16,6 @@ const modallist = {
 };
   
  export default {
-    buttonList,
     modallist
   };
   
